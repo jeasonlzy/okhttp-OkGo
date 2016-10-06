81f0f7a0a3f2efbf0cd027836736e3e5c09331da
jeasonlzy <liaojeason@126.com>
2016-10-07 01:26:42 +0800

    修改readme

--- app/src/main/java/com/lzy/demo/utils/GlideImageLoader.java
+++ app/src/main/java/com/lzy/demo/utils/GlideImageLoader.java
@@ -26,8 +26,8 @@ public class GlideImageLoader implements ImageLoader, NineGridView.ImageLoader {
     @Override
     public void onDisplayImage(Context context, ImageView imageView, String url) {
         Glide.with(context).load(url)//
-                .placeholder(R.drawable.ic_default_image)// 这行貌似是glide的bug,在部分机型上会导致第一次图片不在中间
-                .error(R.drawable.ic_default_image)//
+                .placeholder(R.drawable.ic_default_color)// 这行貌似是glide的bug,在部分机型上会导致第一次图片不在中间
+                .error(R.drawable.ic_default_color)//
                 .diskCacheStrategy(DiskCacheStrategy.ALL)//
                 .into(imageView);
     }
@@ -40,8 +40,8 @@ public class GlideImageLoader implements ImageLoader, NineGridView.ImageLoader {
     @Override
     public void displayImage(Activity activity, String path, ImageView imageView, int width, int height) {
         Glide.with(activity).load(new File(path))//
-                .placeholder(R.drawable.ic_default_image)//
-                .error(R.drawable.ic_default_image)//
+                .placeholder(R.drawable.ic_default_color)//
+                .error(R.drawable.ic_default_color)//
                 .diskCacheStrategy(DiskCacheStrategy.ALL)//
                 .into(imageView);
     }
