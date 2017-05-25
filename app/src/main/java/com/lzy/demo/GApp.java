b590b4ce5779738b3b5f3fa2a6c4a9a912147150
liaoz <liaoz@sxgroup.com>
2017-05-25 13:54:03 +0800

    区分连接超时，读取超时，写入超时常量

--- app/src/main/java/com/lzy/demo/GApp.java
+++ app/src/main/java/com/lzy/demo/GApp.java
@@ -55,9 +55,9 @@ public class GApp extends Application {
                     .debug("OkGo", Level.INFO, true)
 
                     //如果使用默认的 60秒,以下三行也不需要传
-                    .setConnectTimeout(OkGo.DEFAULT_MILLISECONDS)  //全局的连接超时时间
-                    .setReadTimeOut(OkGo.DEFAULT_MILLISECONDS)     //全局的读取超时时间
-                    .setWriteTimeOut(OkGo.DEFAULT_MILLISECONDS)    //全局的写入超时时间
+                    .setConnectTimeout(OkGo.DEFAULT_CONNECT_TIMEOUT)  //全局的连接超时时间
+                    .setReadTimeOut(OkGo.DEFAULT_READ_TIMEOUT)     //全局的读取超时时间
+                    .setWriteTimeOut(OkGo.DEFAULT_WRITE_TIMEOUT)    //全局的写入超时时间
 
                     //可以全局统一设置缓存模式,默认是不使用缓存,可以不传,具体其他模式看 github 介绍 https://github.com/jeasonlzy/
                     .setCacheMode(CacheMode.NO_CACHE)
